79e5836c28d9b8be83eed2def563f06668887401
kajman <kajmaj87@gmail.com>
2021-12-30 13:08:13 +0100

    Added store opcode

--- emucpu.cpp
+++ emucpu.cpp
@@ -5,6 +5,8 @@
 constexpr uint16_t MAX_ADDRESS { 0x10 };
 constexpr uint16_t REGISTERS { 8 };
 
+enum Opcode { LOAD = 0x00, STORE = 0x10, UNINITIALIZED = 0xff };
+
 std::array<std::byte, MAX_ADDRESS> mem;
 uint16_t reg[REGISTERS] = {0};
 
@@ -15,14 +17,18 @@ void initialize_memory(){
 };
 
 void load_program(){
-  mem[0] = std::byte { 0x00 };
+  mem[0] = std::byte {LOAD};
   mem[1] = std::byte { 0x01 };
   mem[2] = std::byte { 0x00 };
   mem[3] = std::byte { 0xa0 };
-  mem[4] = std::byte { 0x00 };
+  mem[4] = std::byte {LOAD};
   mem[5] = std::byte { 0x02 };
   mem[6] = std::byte { 0x01 };
   mem[7] = std::byte { 0x00 };
+  mem[8] = std::byte {STORE};
+  mem[9] = std::byte { 0x01 };
+  mem[10] = std::byte { 0x00 };
+  mem[11] = std::byte { 0x0f };
 };
 
 constexpr uint16_t to_word(const std::byte upper, const std::byte lower) {
@@ -30,13 +36,19 @@ constexpr uint16_t to_word(const std::byte upper, const std::byte lower) {
 };
 
 void process(const std::byte opcode, const std::byte v1, const std::byte v2, const std::byte v3){
-  if (opcode == std::byte{0x00}) {
+  if (opcode == std::byte{ LOAD }) {
      uint8_t register_number = std::to_integer<uint8_t>(v1);
      uint16_t value = to_word(v2, v3);
      reg[register_number] = value;
      std::printf("Loaded %d to r%d\n", value, register_number);
   }
-  if (opcode == std::byte{0xff}) {
+  if (opcode == std::byte{ STORE }) {
+     uint8_t register_number = std::to_integer<uint8_t>(v1);
+     uint16_t memory_address = to_word(v2, v3);
+     mem[memory_address] = std::byte(reg[register_number]);
+     std::printf("Stored %d (%02x) under memory address %04x\n", reg[register_number], reg[register_number], memory_address);
+  }
+  if (opcode == std::byte{ UNINITIALIZED }) {
      std::printf("Uninitialized memory access!\n");
   }
 };
@@ -49,6 +61,9 @@ void print_registers() {
 
 void print_memory() {
   for (int i=0; i<MAX_ADDRESS; i++){
+    if (i % 4 == 0) {
+      std::printf("%04x-%04x: ", i, i + 3);
+    }
     std::printf("%02x ", unsigned(mem[i]));
     if (i % 4 == 3) {
       std::printf("\n");
