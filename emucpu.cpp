6bdb2eb72dde9ca30bd187547cbc3fe3b81f57d1
kajman <kajmaj87@gmail.com>
2021-12-30 10:28:43 +0100

    First simple emulator implementation

---
+++ emucpu.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstddef>
+
+constexpr uint16_t MAX_ADDRESS { 0xff };
+constexpr uint16_t REGISTERS { 8 };
+
+std::byte mem[MAX_ADDRESS] = {std::byte {0xff}};
+uint16_t reg[REGISTERS] = {0};
+
+int pc = 0;
+
+void load_program(){
+  mem[0] = std::byte { 0x00 };
+  mem[1] = std::byte { 0x01 };
+  mem[2] = std::byte { 0x00 };
+  mem[3] = std::byte { 0xa0 };
+  mem[4] = std::byte { 0x00 };
+  mem[5] = std::byte { 0x02 };
+  mem[6] = std::byte { 0x12 };
+  mem[7] = std::byte { 0x0f };
+}
+
+uint16_t to_word(std::byte upper, std::byte lower){
+  return (std::to_integer<uint8_t>(upper) << 8) | std::to_integer<uint8_t>(lower);
+}
+
+void process(std::byte opcode, std::byte v1, std::byte v2, std::byte v3){
+  if (opcode == std::byte{0x00}) {
+     uint8_t register_number = std::to_integer<uint8_t>(v1);
+     uint16_t value = to_word(v2, v3);
+     reg[register_number] = value;
+     std::cout << "Loaded " << value << " to register " << register_number;
+  }
+}
+
+int main() {
+    load_program();
+    while(pc < MAX_ADDRESS) {
+      process(mem[pc], mem[pc+1], mem[pc+2], mem[pc+3]); 
+      pc+=4;
+    }
+    if (pc == MAX_ADDRESS) {
+      std::cout << "Out of memory access!";
+    }
+    return 0;
+}
+
