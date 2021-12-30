8c3485f71c485aa32bc3ae5b0345396e801245eb
kajman <kajmaj87@gmail.com>
2021-12-30 12:48:32 +0100

    Removed most of the constexprs

--- emucpu.cpp
+++ emucpu.cpp
@@ -10,26 +10,26 @@ uint16_t reg[REGISTERS] = {0};
 
 int pc = 0;
 
-constexpr auto initialize_memory = [] {
+void initialize_memory(){
   mem.fill(std::byte{0xff});
 };
 
-constexpr auto load_program = [] {
+void load_program(){
   mem[0] = std::byte { 0x00 };
   mem[1] = std::byte { 0x01 };
   mem[2] = std::byte { 0x00 };
   mem[3] = std::byte { 0xa0 };
   mem[4] = std::byte { 0x00 };
   mem[5] = std::byte { 0x02 };
-  mem[6] = std::byte { 0x12 };
-  mem[7] = std::byte { 0x0f };
+  mem[6] = std::byte { 0x01 };
+  mem[7] = std::byte { 0x00 };
 };
 
-constexpr auto to_word = [] (const std::byte upper, const std::byte lower) -> uint16_t {
+constexpr uint16_t to_word(const std::byte upper, const std::byte lower) {
   return (std::to_integer<uint8_t>(upper) << 8) | std::to_integer<uint8_t>(lower);
 };
 
-constexpr auto process = [] (const std::byte opcode, const std::byte v1, const std::byte v2, const std::byte v3){
+void process(const std::byte opcode, const std::byte v1, const std::byte v2, const std::byte v3){
   if (opcode == std::byte{0x00}) {
      uint8_t register_number = std::to_integer<uint8_t>(v1);
      uint16_t value = to_word(v2, v3);
@@ -41,14 +41,14 @@ constexpr auto process = [] (const std::byte opcode, const std::byte v1, const s
   }
 };
 
-constexpr auto print_registers = [] {
+void print_registers() {
   for (int i=0; i<8; i++){
     std::cout << "r" << i << ": " << reg[i] << "\t";
   }
   std::cout << "\n";
 };
 
-constexpr auto print_memory = [] {
+void print_memory() {
   for (int i=0; i<MAX_ADDRESS; i++){
     std::printf("%02x ", unsigned(mem[i]));
     if (i % 4 == 3) {
